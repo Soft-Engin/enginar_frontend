e69fddf4ceabece9e648ed7bc9398f38b4ca05af
MustafaKrc <mustafa.kirci0@gmail.com>
2024-12-30 23:28:33 +0300

    Upload BlogPage tests

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -3,18 +3,18 @@ import Box from '@mui/material/Box';
 import CommentSection from "./CommentSection";
 import RecommendedUsers from "./RecommendedUsers";
 import UpcomingEvents from "./UpcomingEvents";
-
 import { useSearchParams } from 'react-router-dom'
 
 export default function BlogPage() {
   const [searchParams, setSearchParams] = useSearchParams();
 
+
   return(
-    <Box sx={{ scale: { xs: "70%", sm: "70%", md: "80%", lg: "84%", xl: "100%" }, transformOrigin: "top", width: { lg: "90%", xl: "100%" }, margin: "0 auto" }}>
-      <BlogDetailed blogId={searchParams.get('id')}/>
-      <CommentSection type="blog" contentId={searchParams.get('id')} />
-      <RecommendedUsers />
-      <UpcomingEvents />
+    <Box sx={{ scale: { xs: "70%", sm: "70%", md: "80%", lg: "84%", xl: "100%" }, transformOrigin: "top", width: { lg: "90%", xl: "100%" }, margin: "0 auto" }} data-testid="blog-page-container">
+      <BlogDetailed blogId={searchParams.get('id')} data-testid="blog-detailed"/>
+      <CommentSection type="blog" contentId={searchParams.get('id')} data-testid="comment-section" />
+      <RecommendedUsers data-testid="recommended-users"/>
+      <UpcomingEvents data-testid="upcoming-events"/>
     </Box>
   )
 }
