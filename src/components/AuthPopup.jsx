06710e605794bbd5826ef5c669ae2074d0681d1b
MustafaKrc <mustafa.kirci0@gmail.com>
2024-12-30 22:38:33 +0300

    Create AuthPopup tests

--- src/components/AuthPopup.jsx
+++ src/components/AuthPopup.jsx
@@ -105,9 +105,7 @@ export default function AuthPopup(props) {
           localStorage.setItem("token", response.data.token);
           localStorage.setItem("userLogged", "true");
           props.setUserLogged(true);
-          axios.defaults.headers.common[
-            "Authorization"
-          ] = `Bearer ${response.data.token}`;
+          axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.token}`;
           props.setAnchorElUser(null);
           handleClose();
         }
@@ -129,9 +127,7 @@ export default function AuthPopup(props) {
           error.response.data.message || "An unexpected error occurred."
         );
       } else if (error.request) {
-        setFormError(
-          "Could not connect to the server. Please try again later."
-        );
+        setFormError("Could not connect to the server. Please try again later.");
       } else {
         setFormError("An unexpected error occurred.");
       }
@@ -140,35 +136,46 @@ export default function AuthPopup(props) {
 
   return (
     <React.Fragment>
-      <Stack spacing={2} direction="row" justifyContent={"center"}>
+      <Stack spacing={2} direction="row" justifyContent="center">
         <LoginButton
+          data-testid="open-login-dialog-button"
           variant="contained"
           onClick={() => handleClickOpen(false)}
           id="loginButton"
         >
           Log in
         </LoginButton>
-        <SignupButton variant="contained" onClick={() => handleClickOpen(true)}>
+        <SignupButton
+          data-testid="open-signup-dialog-button"
+          variant="contained"
+          onClick={() => handleClickOpen(true)}
+        >
           Sign up
         </SignupButton>
       </Stack>
 
       <Dialog
+        data-testid="auth-dialog"
         open={open}
         onClose={handleClose}
-        maxWidth={"xs"}
+        maxWidth="xs"
         PaperProps={{
-          sx: { width: { xs: 300, sm: 400, md: 600, lg: 600, xl: 650 }, borderRadius: "16px", backgroundColor: "#C8EFA5" },
+          sx: {
+            width: { xs: 300, sm: 400, md: 600, lg: 600, xl: 650 },
+            borderRadius: "16px",
+            backgroundColor: "#C8EFA5",
+          },
           component: "form",
           onSubmit: handleSubmit,
         }}
       >
-        <DialogTitle fontSize={52} fontWeight={"bold"}>
+        <DialogTitle data-testid="auth-dialog-title" fontSize={52} fontWeight="bold">
           {isSignup ? "Sign Up" : "Log In"}
         </DialogTitle>
-        <DialogContent>
+        <DialogContent data-testid="auth-dialog-content">
           {showSuccess && !isSignup && (
             <Alert
+              data-testid="success-alert"
               severity="success"
               sx={{
                 mb: 2,
@@ -183,14 +190,15 @@ export default function AuthPopup(props) {
             </Alert>
           )}
           {formError && (
-            <Alert severity="error" sx={{ mb: 2 }}>
+            <Alert data-testid="error-alert" severity="error" sx={{ mb: 2 }}>
               {formError}
             </Alert>
           )}
           {isSignup ? (
-            <>
-             <Stack direction="row" spacing={2} sx={{marginBottom:"10px"}}>
+            <React.Fragment>
+              <Stack direction="row" spacing={2} sx={{ marginBottom: "10px" }}>
                 <TextField
+                  data-testid="first-name-input"
                   required
                   margin="dense"
                   id="firstName"
@@ -209,6 +217,7 @@ export default function AuthPopup(props) {
                   }}
                 />
                 <TextField
+                  data-testid="last-name-input"
                   required
                   margin="dense"
                   id="lastName"
@@ -226,8 +235,9 @@ export default function AuthPopup(props) {
                     ),
                   }}
                 />
-                </Stack>
+              </Stack>
               <TextField
+                data-testid="username-input"
                 required
                 margin="dense"
                 id="username"
@@ -246,6 +256,7 @@ export default function AuthPopup(props) {
                 }}
               />
               <TextField
+                data-testid="email-input"
                 required
                 margin="dense"
                 id="email"
@@ -263,9 +274,10 @@ export default function AuthPopup(props) {
                   ),
                 }}
               />
-            </>
+            </React.Fragment>
           ) : (
             <TextField
+              data-testid="identifier-input"
               required
               margin="dense"
               id="identifier"
@@ -286,6 +298,7 @@ export default function AuthPopup(props) {
           )}
 
           <TextField
+            data-testid="password-input"
             required
             margin="dense"
             id="password"
@@ -306,6 +319,7 @@ export default function AuthPopup(props) {
 
           {isSignup && (
             <TextField
+              data-testid="confirm-password-input"
               required
               margin="dense"
               id="confirmPassword"
@@ -328,6 +342,7 @@ export default function AuthPopup(props) {
           {!isSignup && (
             <Typography noWrap component="div" color="#535353">
               <Link
+                data-testid="forgot-password-link"
                 onClick={() => console.log("Forgot Password")}
                 color="#4B9023"
                 underline="hover"
@@ -339,18 +354,20 @@ export default function AuthPopup(props) {
           )}
         </DialogContent>
         <DialogActions
+          data-testid="auth-dialog-actions"
           sx={{
             display: "flex",
             justifyContent: "center",
             flexDirection: "column",
           }}
         >
-          <SubmitButton type="submit">
+          <SubmitButton data-testid="auth-submit-button" type="submit">
             {isSignup ? "Sign Up" : "Log In"}
           </SubmitButton>
           <Typography noWrap component="div" color="#535353">
             {isSignup ? "Already have an account? " : "Don't have an account? "}
             <Link
+              data-testid="switch-mode-link"
               onClick={isSignup ? handleSwitchToLogin : handleSwitchToSignup}
               color="#4B9023"
               underline="hover"
