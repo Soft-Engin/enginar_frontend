c89f29bf205405ef118ec96eefad751fddc562ad
g-akca <gok.akca66@gmail.com>
2024-12-31 17:34:05 +0300

    Minor BlogPage fix

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -10,9 +10,11 @@ export default function BlogPage() {
   const [searchParams, setSearchParams] = useSearchParams();
 
   return(
-    <Box sx={{ scale: { xs: "70%", sm: "70%", md: "80%", lg: "84%", xl: "100%" }, transformOrigin: "top", width: { lg: "90%", xl: "100%" }, margin: "0 auto" }}>
-      <BlogDetailed blogId={searchParams.get('id')}/>
-      <CommentSection type="blog" contentId={searchParams.get('id')} />
+    <Box sx={{ m: 4, maxWidth: 800 }}>
+      <Box sx={{ scale: { xs: "70%", sm: "70%", md: "80%", lg: "84%", xl: "100%" }, transformOrigin: "top", width: { lg: "90%", xl: "100%" }, margin: "0 auto" }}>
+        <BlogDetailed blogId={searchParams.get('id')}/>
+        <CommentSection type="blog" contentId={searchParams.get('id')} />
+      </Box>
       <RecommendedUsers />
       <UpcomingEvents />
     </Box>
