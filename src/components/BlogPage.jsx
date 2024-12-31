87f8d055f02d4f09ef28a7d4b1db515618344b99
g-akca <gok.akca66@gmail.com>
2024-12-31 21:00:02 +0300

    Fixed BlogDetailed width + made SearchBar responsive

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -4,16 +4,23 @@ import CommentSection from "./CommentSection";
 import RecommendedUsers from "./RecommendedUsers";
 import UpcomingEvents from "./UpcomingEvents";
 
-import { useSearchParams } from 'react-router-dom'
+import { useSearchParams } from "react-router-dom";
 
 export default function BlogPage() {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  return(
-    <Box sx={{ m: 4, maxWidth: 800 }}>
-      <Box sx={{ scale: { xs: "70%", sm: "70%", md: "80%", lg: "84%", xl: "100%" }, transformOrigin: "top", width: { lg: "90%", xl: "100%" }, margin: "0 auto" }}>
-        <BlogDetailed blogId={searchParams.get('id')}/>
-        <CommentSection type="blog" contentId={searchParams.get('id')} />
+  return (
+    <Box sx={{ m: 4, width: 800 }}>
+      <Box
+        sx={{
+          scale: { xs: "70%", sm: "70%", md: "90%", lg: "84%", xl: "100%" },
+          transformOrigin: "top",
+          width: { md: "90%", lg: "90%", xl: "100%" },
+          margin: "0 auto",
+        }}
+      >
+        <BlogDetailed blogId={searchParams.get("id")} />
+        <CommentSection type="blog" contentId={searchParams.get("id")} />
       </Box>
       <RecommendedUsers />
       <UpcomingEvents />
